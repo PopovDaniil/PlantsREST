b5e83837a118a641e03c8798660d6cb6dd741973
Popov Daniil <pde-rnd@yandex.ru>
2020-10-29 19:57:52 +0300

    Добавлен IP

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ fastify.route({
     }
 })
 
-fastify.listen(PORT, (err,address) => {
+fastify.listen(PORT,"0.0.0.0", (err,address) => {
     console.error(PORT);
     fastify.log.info(`server listening on ${address}`)
 })
